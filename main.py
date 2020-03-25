f8a575a3f02bf81a7065c0097d3f8c7556743e43
Dipesh <dipesh.pandey42@gmail.com>
2020-03-25 20:58:55 +0545

    Indentation resolved

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ def death_global():
  	cases_list = []
  	for res in result:
  		cases_list.append(res.text)
-    return "There are"+cases_list[0]+" Total cases out of which"+cases_list[1]+" have died and"+cases_list[2]+" have recovered ."
+
+ 	return "There are"+cases_list[0]+" Total cases out of which"+cases_list[1]+" have died and"+cases_list[2]+" have recovered ."
 
 @app.route("/death/country/<id>")
 def death_country(id):
@@ -34,6 +35,7 @@ def death_country(id):
     cases_list = []
     for res in result:
     	cases_list.append(res.text)
+
     return "In " +idu+" There are"+cases_list[0]+"Total cases out of which"+cases_list[1]+"are dead and"+cases_list[2]+"have already recovered . There are still "+active_cases+ " active cases ."
 
 if __name__ == '__main__':
