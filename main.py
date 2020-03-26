6af5745ca55d4fc4293ceb920b4535f8306e7354
Dipesh <dipesh.pandey42@gmail.com>
2020-03-26 11:04:15 +0545

    Minor indentation error resolved

--- main.py
+++ main.py
@@ -17,30 +17,13 @@ def hello():
 def death_global():
 	page = requests.get("https://www.worldometers.info/coronavirus/")
 	soup = BeautifulSoup(page.content, 'html.parser')
- 	result = soup.find_all("div", {"class":"maincounter-number"})
+	result = soup.find_all("div", {"class":"maincounter-number"})
  	cases_list = []
  	for res in result:
  		cases_list.append(res.text)
 
  	return "There are"+cases_list[0]+" Total cases out of which"+cases_list[1]+" have died and"+cases_list[2]+" have recovered ."
 
-@app.route('/get_country_detail', methods=['POST'])
-def get_country_detail():
-    data = request.get_json(silent=True)
-    # print (data)
-    country = data['queryResult']['parameters']['geo-country-code']['name']
-    print (country)
-
-    if (country =="world"):
-    	response = death_global()
-    else:
-    	response = death_country(country)
-    
-    reply = { "fulfillmentText": response }
-    
-    return jsonify(reply)
-
-# @app.route("/death/country/<id>")
 def death_country(id):
     idu = id.upper()
     page = requests.get("https://www.worldometers.info/coronavirus/country/"+id+"/")
@@ -55,6 +38,24 @@ def death_country(id):
     	cases_list.append(res.text)
 
     return "In " +idu+" There are"+cases_list[0]+"Total cases out of which"+cases_list[1]+"are dead and"+cases_list[2]+"have already recovered . There are still "+active_cases+ " active cases ."
+
+@app.route('/get_country_detail', methods=['POST'])
+def get_country_detail():
+	data = request.get_json(silent=True)
+    # print (data)
+    country = data['queryResult']['parameters']['geo-country-code']['name']
+    print (country)
+
+    if (country =="world"):
+    	response = death_global()
+    else:
+    	response = death_country(country)
+    
+    reply = { "fulfillmentText": response }
+    
+    return jsonify(reply)
+
+# @app.route("/death/country/<id>")
 
 if __name__ == '__main__':
     app.run()
