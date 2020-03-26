8a2affdd615fd2def4212257b560061227b4606e
Dipesh <dipesh.pandey42@gmail.com>
2020-03-26 10:56:02 +0545

    Diffent functions for global and country

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify, request
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://www.worldometers.info/coronavirus/")
-soup = BeautifulSoup(page.content, 'html.parser')
+# page = requests.get("https://www.worldometers.info/coronavirus/")
+# soup = BeautifulSoup(page.content, 'html.parser')
 
 # Initialize application
 app = Flask(__name__)
@@ -13,8 +13,10 @@ app = Flask(__name__)
 def hello():
     return "Flask setup"
 
-@app.route("/death/global")
+# @app.route("/death/global")
 def death_global():
+	page = requests.get("https://www.worldometers.info/coronavirus/")
+	soup = BeautifulSoup(page.content, 'html.parser')
  	result = soup.find_all("div", {"class":"maincounter-number"})
  	cases_list = []
  	for res in result:
@@ -23,27 +25,22 @@ def death_global():
  	return "There are"+cases_list[0]+" Total cases out of which"+cases_list[1]+" have died and"+cases_list[2]+" have recovered ."
 
 @app.route('/get_country_detail', methods=['POST'])
-def get_movie_detail():
+def get_country_detail():
     data = request.get_json(silent=True)
-    print (data)
+    # print (data)
     country = data['queryResult']['parameters']['geo-country-code']['name']
     print (country)
-    response = "Could not get " +country+ " death detail at the moment, please try again"
+
+    if (country =="world"):
+    	response = death_global()
+    else:
+    	response = death_country(country)
     
     reply = { "fulfillmentText": response }
     
     return jsonify(reply)
 
-# @app.route('/get_country_detail/country/<id>', methods=['POST'])
-# def get_movie_detail(id):
-#     data = request.get_json(silent=True)
-#     response = "Could not get "+id+" country detail at the moment, please try again"
-    
-#     reply = { "fulfillmentText": response }
-    
-#     return jsonify(reply)
-
-@app.route("/death/country/<id>")
+# @app.route("/death/country/<id>")
 def death_country(id):
     idu = id.upper()
     page = requests.get("https://www.worldometers.info/coronavirus/country/"+id+"/")
