bdb3afe5a7242069a0e8ccdb2a7610fa3172d422
Dipesh <dipesh.pandey42@gmail.com>
2020-03-26 11:19:39 +0545

    Indentation errors

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask setup"
+		return "Flask setup"
 
 # @app.route("/death/global")
 def death_global():
@@ -19,39 +19,39 @@ def death_global():
 	soup = BeautifulSoup(page.content, 'html.parser')
 	result = soup.find_all("div", {"class":"maincounter-number"})
 	cases_list = []
- 	for res in result:
- 		cases_list.append(res.text)
+	for res in result:
+		cases_list.append(res.text)
 
- 	return "There are"+cases_list[0]+" Total cases out of which"+cases_list[1]+" have died and"+cases_list[2]+" have recovered ."
+	return "There are"+cases_list[0]+" Total cases out of which"+cases_list[1]+" have died and"+cases_list[2]+" have recovered ."
 
 def death_country(id):
-    idu = id.upper()
-    page = requests.get("https://www.worldometers.info/coronavirus/country/"+id+"/")
-    soup = BeautifulSoup(page.content, 'html.parser')
-    result = soup.find_all("div", {"class":"maincounter-number"})
-    
-    active = soup.find("div", {"class":"number-table-main"})
-    active_cases = active.text
-    cases_list = []
-    for res in result:
-    	cases_list.append(res.text)
+		idu = id.upper()
+		page = requests.get("https://www.worldometers.info/coronavirus/country/"+id+"/")
+		soup = BeautifulSoup(page.content, 'html.parser')
+		result = soup.find_all("div", {"class":"maincounter-number"})
+		
+		active = soup.find("div", {"class":"number-table-main"})
+		active_cases = active.text
+		cases_list = []
+		for res in result:
+			cases_list.append(res.text)
 
-    return "In " +idu+" There are"+cases_list[0]+"Total cases out of which"+cases_list[1]+"are dead and"+cases_list[2]+"have already recovered . There are still "+active_cases+ " active cases ."
+		return "In " +idu+" There are"+cases_list[0]+"Total cases out of which"+cases_list[1]+"are dead and"+cases_list[2]+"have already recovered . There are still "+active_cases+ " active cases ."
 
 @app.route('/get_country_detail', methods=['POST'])
 def get_country_detail():
-    data = request.get_json(silent=True)
-    # print (data)
-    country = data['queryResult']['parameters']['geo-country-code']['name']
-    print (country)
+		data = request.get_json(silent=True)
+		# print (data)
+		country = data['queryResult']['parameters']['geo-country-code']['name']
+		print (country)
 
-    if (country =="world"):
-    	response = death_global()
-    else:
-    	response = death_country(country)
-    
-    reply = { "fulfillmentText": response }    
-    return jsonify(reply)
+		if (country =="world"):
+			response = death_global()
+		else:
+			response = death_country(country)
+		
+		reply = { "fulfillmentText": response }    
+		return jsonify(reply)
 
 if __name__ == '__main__':
-    app.run()
+		app.run()
