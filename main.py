bf1660f712312e31f55728b6bba68b83ddbdcbee
dipesh pandey <dipesh.pandey42@gmail.com>
2020-03-28 10:44:25 +0545

    Update main.py

--- main.py
+++ main.py
@@ -54,11 +54,11 @@ def death_country(id):
 @app.route('/get_country_detail', methods=['POST'])
 def get_country_detail():
 		data = request.get_json(silent=True)
-		response = death_global()
-# 		intent = data['intent']['displayName']
-# 		print (intent)
-# 		if(intent == "nepal data int"):
-# 			response = "Calling API to get nepali Data"
+# 		response = death_global()
+		intent = data['queryResult']['intent']['displayName']
+		print (intent)
+		if(intent == "nepal data int"):
+			response = "Calling API to get nepali Data"
 
 		# print (data)
 		# query_text = data['queryResult']['queryText']
@@ -70,8 +70,8 @@ def get_country_detail():
 
 		# if (country =="world"):
 		# 	response = death_global()
-# 		else:
-# 			response = death_global()
+		else:
+			response = death_global()
 		
 		reply = { "fulfillmentText": response }    
 		return jsonify(reply)
