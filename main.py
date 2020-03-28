64b5f029df15c96532d55fb5162c09adc61ec9e7
Dipesh <dipesh.pandey42@gmail.com>
2020-03-28 17:22:57 +0545

    Minor changes

--- main.py
+++ main.py
@@ -77,61 +77,63 @@ def get_country_detail():
     	data2 = data[1]
     	data3 = data[2]
     	response1 = [
-      	{
-  "facebook": {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type":"generic",
-        "elements":[
-           {
-            "title":"Here is your title!",
-            "image_url":"https://i.imgur.com/fFxXB2m.png",
-            "subtitle":"Here goes your subtitle.",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://google.com",
-              "webview_height_ratio": "tall"
+  {
+    "facebook": {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [
+            {
+              "title": "Here is your title!",
+              "image_url": "https://i.imgur.com/fFxXB2m.png",
+              "subtitle": "Here goes your subtitle.",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://google.com",
+                "webview_height_ratio": "tall"
+              },
+              "buttons": [
+                {
+                  "type": "web_url",
+                  "url": "https://facebook.com",
+                  "title": "Link to Facebook"
+                },
+                {
+                  "type": "postback",
+                  "title": "Some Text",
+                  "payload": "Some Text"
+                }
+              ]
             },
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://facebook.com",
-                "title":"Link to Facebook"
-              },{
-                "type":"postback",
-                "title":"Some Text",
-                "payload":"Some Text"
-              }              
-            ]      
-          },
-          {
-            "title":"Here is your title!",
-            "image_url":"https://i.imgur.com/fFxXB2m.png",
-            "subtitle":"Here goes your subtitle.",
-            "default_action": {
-              "type": "web_url",
-              "url": "https://google.com",
-              "webview_height_ratio": "tall"
-            },
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://facebook.com",
-                "title":"Link to Facebook"
-              },{
-                "type":"postback",
-                "title":"Some Text",
-                "payload":"Some Text"
-              }              
-            ]      
-          }
-        ]      
+            {
+              "title": "Here is your title!",
+              "image_url": "https://i.imgur.com/fFxXB2m.png",
+              "subtitle": "Here goes your subtitle.",
+              "default_action": {
+                "type": "web_url",
+                "url": "https://google.com",
+                "webview_height_ratio": "tall"
+              },
+              "buttons": [
+                {
+                  "type": "web_url",
+                  "url": "https://facebook.com",
+                  "title": "Link to Facebook"
+                },
+                {
+                  "type": "postback",
+                  "title": "Some Text",
+                  "payload": "Some Text"
+                }
+              ]
+            }
+          ]
+        }
       }
     }
   }
-}
-    ]
+]
       
      reply = { "fulfillmentMessage": response1 }
       
