5a53af3df62751d9bc1efdf770499f700ef9e629
Dipesh <dipesh.pandey42@gmail.com>
2020-03-28 17:04:41 +0545

    Minor bugs

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def get_country_detail():
 }
     ]
       
-      reply ={"fulfillmentMessages": response1 }
+     reply ={"fulfillmentMessages": response1 }
       
     # print (data)
     # query_text = data['queryResult']['queryText']
