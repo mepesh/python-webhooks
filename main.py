3bc7ef4d74a70ea5013fcc7ae6657031203e2700
Dipesh <dipesh.pandey42@gmail.com>
2020-05-11 18:26:42 +0545

    Add

--- main.py
+++ main.py
@@ -401,7 +401,7 @@ def get_country_detail():
         },
         {
           "text":{"text":["Dummy text"]}
-        }
+        },
         {
           "card": {
             "title": "Covid-19 Nepal",
